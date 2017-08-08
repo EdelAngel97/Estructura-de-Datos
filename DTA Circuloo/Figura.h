5a5c61633c5384663db5a13b738f6d665973d857
EdelAngel97 <esteban-9709@hotmail.com>
2017-08-07 23:01:09 -0500

    Create Figura.h

---
+++ DTA Circuloo/Figura.h
@@ -0,0 +1,14 @@
+
+class Figura{
+public:
+	Figura();
+	Figura(int, int);
+	int getArea();
+	int getPerimetro();
+	void setArea(int);
+	void setPerimetro(int);
+
+protected:
+	int area;
+	int perimetro;
+}; 
